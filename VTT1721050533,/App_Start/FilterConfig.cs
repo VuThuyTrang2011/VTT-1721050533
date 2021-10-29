e1cee260fbed13b9926e5778d655ec75dcb46669
VU THUY TRANG <1721050533@student.humg.edu.vn>
2021-10-29 08:06:43 +0700

    Add project files.

---
+++ VTT1721050533,/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VTT1721050533_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ VTT1721050533,/Models/QL.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VTT1721050533_.Models
+{
+    public class QL : DbContext
+    {
+        public QL() : base("name=QL")
+        {
+        }
+
+        public DbSet<Lecture> Lectures { get; set; }
+        public virtual DbSet<Person> Person { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Lecture>()
+                .Property(e => e.PersonID)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Lecture>()
+                .Property(e => e.PersonName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Lecture>()
+                .Property(e => e.Faculty)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Lecture>()
+                .Property(e => e.Department)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Person>()
+                .Property(e => e.PersonID)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Person>()
+                .Property(e => e.PersonName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Student>()
+                .Property(e => e.PersonID)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Student>()
+                .Property(e => e.PersonName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Student>()
+                .Property(e => e.University)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Student>()
+                .Property(e => e.Address)
+                .IsUnicode(false);
+        }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal global::System.Object Entry(Student student)
+        {
+        }
+
+        internal object Entry(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
